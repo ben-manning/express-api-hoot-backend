bb1f46f132078dd3577412ee2b743796ee12a363
Ben Manning <bmanning@me.com>
2025-03-17 13:55:19 -0400

    completed CRUD functionality

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -33,7 +33,7 @@ router.get('/', verifyToken, async (req, res) => {
 // GET - /hoots/:hootId
 router.get('/:hootId', verifyToken, async (req, res) => {
   try {
-    const hoot = await Hoot.findById(req.params.hootId).populate('author');
+    const hoot = await Hoot.findById(req.params.hootId).populate(['author', 'comments.author']);
     res.status(200).json(hoot);
   } catch (err) {
     res.status(500).json({ err: err.message });
@@ -58,6 +58,42 @@ router.put('/:hootId', verifyToken, async (req, res) => {
     updatedHoot._doc.author = req.user;
 
     res.status(200).json(updatedHoot);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+// DELETE /hoots/:hootId
+router.delete('/:hootId', verifyToken, async (req, res) => {
+  try {
+    const hoot = await Hoot.findById(req.params.hootId);
+
+    if (!hoot.author.equals(req.user._id)) {
+      return res.status(403).send('You are not allowed to do that!');
+    }
+
+    const deletedHoot = await Hoot.findByIdAndDelete(req.params.hootId);
+    res.status(200).json(deletedHoot);
+
+
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+// POST /hoots/:hootId/comments
+router.post('/:hootId/comments', verifyToken, async (req, res) => {
+  try {
+    req.body.author = req.user._id;
+    const hoot = await Hoot.findById(req.params.hootId);
+    hoot.comments.push(req.body);
+    await hoot.save();
+
+    const newComment = hoot.comments[hoot.comments.length - 1];
+
+    newComment._doc.author = req.user;
+
+    res.status(200).json(newComment);
   } catch (err) {
     res.status(500).json({ err: err.message });
   }
